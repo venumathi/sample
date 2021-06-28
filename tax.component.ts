94a424b44c9aac7b16a4ad8bb24b1969bc0f26c7
venumathi <45119687+venumathi@users.noreply.github.com>
2021-06-28 14:53:16 +0530

    check file upload

---
+++ tax.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'centizen-cart-tax',
+  templateUrl: './tax.component.html',
+  styleUrls: ['./tax.component.css']
+})
+export class TaxComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
